9df1bb251b01fe7f3805950e25d6e4db63738f90
digiguru <digiguru@gmail.com>
2014-10-27 22:54:39 +0000

    Added grunt task to transpile ES6 to modules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,11 @@ module.exports = function (grunt) {
 
     matchdep.filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
+    
+    //grunt.loadNpmTasks('grunt-es6-module-transpiler');
+
+
+    
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
         buildDest: "./dist",
@@ -14,13 +19,53 @@ module.exports = function (grunt) {
         ],
 
         uglify: require("./build_tasks/uglify/uglify.js"),
+        transpile: {
+            //main: {
+            //    type: "amd",//"cjs", // or "amd" or "yui"
+            //    files: [{
+            //        expand: true,
+            //        cwd: 'src/',
+            //        src: ['**/*.ES6.js'],
+            //        dest: 'tmp/'
+            //    }]
+            //}
+                     toCJS: {
+        type: "cjs",
+        files: {
+          'tmp/clickago.commonjs.js': ['src/clickago.ES6.js']
+        },
+      },
+      toAMD: {
+        type: "amd",
+        files: {
+          'tmp/clickago.amd.js': ['src/clickago.ES6.js']
+        }
+      },
+      toYUI: {
+        type: "yui",
+        files: {
+          'tmp/clickago.yui.js': ['src/clickago.ES6.js']
+        }
+      },
+      toGlobals: {
+        type: "globals",
+        imports: { bar: "Clickago" },
+        files: {
+          'tmp/clickago.globals.js': ['src/clickago.ES6.js']
+        }
+      },
+            
+        },
+        
         jsdoc: require("./build_tasks/jsdoc/jsdoc.js"),
+        
         watch: require("./build_tasks/watch/watch.js")
+        
     });
-
+ 
     grunt.registerTask("start", ["build", "watch"]);
 
-    grunt.registerTask("build", ["uglify", "jsdoc"]);
+    grunt.registerTask("build", ['transpile', "uglify", "jsdoc"]);
 
     grunt.registerTask("default", ["start"]);
 };
---
+++ tmp/clickago.yui.js
@@ -0,0 +1,163 @@
+YUI.add("src/clickago.ES6", function(Y, NAME, __imports__, __exports__) {
+    "use strict";
+    /*jslint esnext:true */
+    /**
+         * Clickago constructor.
+         *
+         * @class Clickago
+         */
+    function Clickago() {
+        var Action, _call;
+
+        this.actions = [];
+
+        this.rollbacks = [];
+
+        this.canRedo = null;
+
+        this.canUndo = null;
+        
+        /**
+         * @method register
+         * @memberOf Clickago
+         * @param {Object} actionOptions
+         * @param {Function} actionOptions.method - Reference to the function that will be returned after registration.
+         * @param {*=} actionOptions.thisArg - Value of `this` in the `method`.
+         * @param {Array=} actionOptions.arguments - An array of arguments.
+         * @param {Object} rollbackOptions
+         * @param {Function} rollbackOptions.method - Reference to the function that will be registered as an undo.
+         * @param {*=} rollbackOptions.thisArg - Value of `this` in the `method`.
+         * @param {Array=} rollbackOptions.arguments - An array of arguments.
+         * @returns {Function} - Returns function bound with `this` and `arguments`.
+         */
+        this.register = function (actionOptions, rollbackOptions) {
+            this.actions[this.actions.length] = new Action({
+                rollback: rollbackOptions,
+                action: actionOptions
+            });
+
+            // Clear `rollbacks` array every time new action is registered.
+            this.rollbacks.length = 0;
+
+            this.canRedo = false;
+
+            this.canUndo = !this.disabled;
+        };
+
+        /**
+         * This function gets the latest registered actions and calls it.
+         *
+         * @method undo
+         * @memberOf Clickago
+         * @returns {*}
+         */
+        this.undo = function () {
+            var action;
+
+            if (this.disabled || !this.canUndo) {
+                return;
+            }
+
+            action = this.actions[this.actions.length - 1];
+
+            this.rollbacks[this.rollbacks.length] = action;
+
+            this.canRedo = true;
+
+            this.actions.length--;
+
+            this.canUndo = !!this.actions.length;
+
+            return _call(action.rollback);
+        };
+
+        /**
+         * This function gets the latest registered rollback and calls it.
+         *
+         * @method redo
+         * @memberOf Clickago
+         * @returns {*}
+         */
+        this.redo = function () {
+            var action;
+
+            if (this.disabled || !this.canRedo) {
+                return;
+            }
+
+            action = this.rollbacks[this.rollbacks.length - 1];
+
+            this.actions[this.actions.length] = action;
+
+            this.canUndo = true;
+
+            this.rollbacks.length--;
+
+            this.canRedo = !!this.rollbacks.length;
+
+            return _call(action.action);
+        };
+
+        /**
+         * Disables Clickago undo/redo methods.
+         *
+         * @method disable
+         * @memberOf Clickago
+         */
+        this.disable = function () {
+            this.disabled = true;
+
+            this.canUndo = false;
+
+            this.canRedo = false;
+        };
+
+        /**
+         * Enables Clickago undo/redo methods.
+         *
+         * @method enable
+         * @memberOf Clickago
+         */
+        this.enable = function () {
+            this.disabled = false;
+
+            this.canUndo = !!this.actions.length;
+
+            this.canRedo = !!this.rollbacks.length;
+        };
+
+        /**
+         * Models
+         *
+         * @namespace Models
+         * @memberOf Clickago
+         */
+
+        /**
+         * Data model for actions.
+         *
+         * @class Action
+         * @memberOf Clickago.Models
+         */
+        Action = function (options) {
+            this.action = options.action;
+            this.rollback = options.rollback;
+        };
+
+        /**
+         * Simple method for calling actions and rollbacks.
+         *
+         * @method _call
+         * @memberOf Clickago
+         * @param actionOptions
+         * @returns {*}
+         * @private
+         */
+        _call = function (actionOptions) {
+            return actionOptions.method.apply(actionOptions.thisArg, actionOptions.arguments);
+        };
+
+    }
+    __exports__.Clickago = Clickago;
+    return __exports__;
+}, "@VERSION@", {"es":true,"requires":[]});
